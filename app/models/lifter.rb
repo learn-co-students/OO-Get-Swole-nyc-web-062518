0474a200bf33763415953bb67049265458e4ec53
github username <github email address>
2018-07-11 17:08:28 -0400

    Done.

--- app/models/lifter.rb
+++ app/models/lifter.rb
@@ -1,16 +1,51 @@
+require 'pry'
 
 class Lifter
 
-
+  @@all = []
 
   attr_reader :name, :lift_total
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def memberships
+    Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms
+    memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  def self.average_lift_total
+    total = 0
+    self.all.each do |lifter|
+      total =+ lifter.lift_total
+    end
+    total/self.all.count
+  end
+
+  def total_cost
+    total = 0
+    memberships.each do |membership|
+      total += membership.cost
+    end
+    total
+  end
+
+  def sign_lifter_to_new_gym(membership_cost, gym)
+    Member.new(membership_cost, gym, self)
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -5,4 +5,15 @@ def reload
 end
 
 
+nysc = Gym.new("NYSC")
+equinox = Gym.new("Equinox")
+
+ann = Lifter.new("Ann", 100)
+jack = Lifter.new("Jack", 130)
+rose = Lifter.new("Rose", 200)
+
+member1 = Membership.new(100, nysc, ann)
+member2 = Membership.new(130, equinox, jack)
+member3 = Membership.new(200, equinox, rose)
+
 binding.pry
