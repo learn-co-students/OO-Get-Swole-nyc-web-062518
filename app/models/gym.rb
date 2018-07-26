92cba9fd35546a8c531176dfc56f662181618bca
yunglleung1 <yungl.leung1@gmail.com>
2018-07-26 09:17:53 -0400

    Done.

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -6,11 +6,36 @@ class Gym
   def initialize(name)
     @name = name
     ALL << self
-
   end
 
-  def self.all
+  def self.all # Get a list of all gyms
     ALL
   end
 
+  def memberships #returns an array of all of that gym's memberships
+    Membership.all.select do |membership|
+      membership.gym == self
+    end
+  end
+
+  def lifters #returns an array of all of that gym's lifters
+    memberships.map do |membership|
+      membership.lifter
+    end
+  end
+
+  def lifter_names #returns an array of all the names of that gym's lifters
+    memberships.map do |membership|
+      membership.lifter.name
+    end
+
+  end
+
+  def lift_total_for_gym #returns a sum of lift_totals for all the lifters at that gym
+    lift_total_sum = 0
+    lifters.each do |lifter|
+      lift_total_sum += lifter.lift_total
+    end
+    lift_total_sum
+  end
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,12 +1,17 @@
 class Membership
+  ALL = []
 
+  attr_reader :lifter, :gym, :cost
 
-  attr_reader :cost
-
-  def initialize(cost)
+  def initialize(lifter, gym, cost)
+    @lifter = lifter
+    @gym = gym
     @cost = cost
+    ALL << self
   end
 
-
+  def self.all # Get a list of all memberships
+    ALL
+  end
 
 end
